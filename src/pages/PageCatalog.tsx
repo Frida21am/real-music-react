b42a11b78216aa569b02df6c42cf93780b3f4732
Alexandra <Frida21am@users.noreply.github.com>
2024-06-17 20:25:09 +0300

    Сделала попап формы обратной связи на главном фуллскрине

--- src/pages/PageCatalog.tsx
+++ src/pages/PageCatalog.tsx
@@ -1,19 +1,10 @@
+import MainFullScreen from "../components/MainFullScreen.tsx"
 import Products from "../entities/Products.tsx"
 
 function PageCatalog() {
   return (
     <div className="catalog">
-      <div className="fullscreen">
-        <div className="container">
-          <div className="fulscreen-caption">
-            <h1 className="fulscreen-caption__title">Real Music</h1>
-            <h2 className="fulscreen-caption__subtitle">Музыкальные инструменты в Москве</h2>
-            <div className="fulscreen-caption__btn">
-              <a href="#catalog">Получить консультацию</a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <MainFullScreen />
 
       <div className="container">
         <Products />
