4141f88f7b762a2295ce415a6b9da15ad3725615
Alexandra <Frida21am@users.noreply.github.com>
2024-07-11 19:20:21 +0300

    Вернула верстку 404 страницы

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,46 +1,36 @@
 import * as React from "react";
-import { Link, HeadFC, PageProps } from "gatsby";
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
+import { HeadFC, PageProps } from "gatsby";
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <div className="notfound">
+      <div className="notfound-banner">
+        <div className="notfound-banner-text">
+          <h3 className="notfound-banner-text__title">
+            Упс, неправильный путь!
+          </h3>
+          <p className="notfound-banner-text__caption">
+            Страница, которую вы ищете, не найдена.
+          </p>
+        </div>
+      </div>
+      <div className="notfound-links">
+        <div className="notfound-link notfound-link__tocatalog">
+          <a href="/">
+            <p className="notfound-link-caption notfound-link-caption__tocatalog">
+              В каталог
+            </p>
+          </a>
+        </div>
+        <div className="notfound-link notfound-link__tofaq">
+          <a href="/">
+            <p className="notfound-link-caption notfound-link-caption__tofaq">
+              Часто задаваемые вопросы
+            </p>
+          </a>
+        </div>
+      </div>
+    </div>
   );
 };
 
